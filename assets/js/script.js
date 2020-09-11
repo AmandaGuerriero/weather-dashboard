e723f24772f48c21841a4d747b06747845566106
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-11 14:53:42 -0700

    Saving city searches to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Global Variables
+var cityNameEl = document.getElementById("city");
+var searchBtn = document.getElementById("search-btn");
+
 // Fetch current weather
 fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=Lindenhurst&appid=abc282c13673b28882968001a1c14445"
@@ -8,9 +12,11 @@ fetch(
 .then(function(response) {
     var responseContainerEl = document.querySelector("#response-container");
     var currentWeather = document.createElement("div");
-    currentWeather.setAttribute("p", response.main.temp)
+    currentWeather.setAttribute("p", response.main.temp);
     responseContainerEl.appendChild(currentWeather);
     console.log(currentWeather);
+    // How to make it show on the page?
+    // How to convert the units?
 })
 
 // Show current weather
@@ -23,4 +29,23 @@ fetch(
 // Future 5-day weather for the city
 
 // City search is added to history
+function saveCity() {    
+    var cityName = cityNameEl.value.trim();
+    // If city is not empty
+    if (city !== "") {
+        var citySave =
+        JSON.parse(window.localStorage.getItem("citySave")) || [];
+        
+        var cityInfo = {
+            name: cityName
+            // Not sure what to put ^^
+        };
 
+        // Save to Local Storage
+        citySave.push(cityInfo);
+        window.localStorage.setItem("citySave", JSON.stringify(citySave));
+    }
+}
+
+// Submit City
+searchBtn.onclick = saveCity;
