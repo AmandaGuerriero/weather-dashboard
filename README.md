c4b6b65d9acf2874d694a31cedc60ac207bdc6ef
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-11 17:18:28 -0700

    styled HTML more

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # weather-dashboard
 
 API Key: abc282c13673b28882968001a1c14445
+
+var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,8 @@
 // Global Variables
 var cityNameEl = document.getElementById("city");
 var searchBtn = document.getElementById("search-btn");
+var apiKey = "abc282c13673b28882968001a1c14445"
 
-// Fetch current weather
-fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Lindenhurst&appid=abc282c13673b28882968001a1c14445"
-)
-.then(function(response) {
-    return response.json();
-})
-.then(function(response) {
-    var responseContainerEl = document.querySelector("#response-container");
-    var currentWeather = document.createElement("div");
-    currentWeather.setAttribute("p", response.main.temp);
-    responseContainerEl.appendChild(currentWeather);
-    console.log(currentWeather);
-    // How to make it show on the page?
-    // How to convert the units?
-})
 
 // Show current weather
 
@@ -29,7 +14,8 @@ fetch(
 // Future 5-day weather for the city
 
 // City search is added to history
-function saveCity() {    
+function saveCity(event) {    
+    event.preventDefault();
     var cityName = cityNameEl.value.trim();
     // If city is not empty
     if (city !== "") {
@@ -38,14 +24,55 @@ function saveCity() {
         
         var cityInfo = {
             name: cityName
-            // Not sure what to put ^^
         };
 
         // Save to Local Storage
         citySave.push(cityInfo);
         window.localStorage.setItem("citySave", JSON.stringify(citySave));
     }
+    console.log(cityName)
+    getCity(cityName);
+}
+
+function getCity(cityName) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+    )
+    .then(function(response) {
+        response.json();
+        console.log(response);
+    })
+    .then(function(data) {
+        console.log(data);
+        var responseContainerEl = document.querySelector("#response-container");
+        var currentWeather = document.createElement("div");
+        currentWeather.setAttribute("p", data.main.temp);
+        responseContainerEl.appendChild(currentWeather);
+        console.log(currentWeather);
+        // How to make it show on the page?
+        // How to convert the units?
+    })
 }
 
 // Submit City
 searchBtn.onclick = saveCity;
+
+
+
+// Show all Cities
+function printCitySearches() {
+    var citySearch = JSON.parse(window.localStorage.getItem("citySave")) || [];
+     
+    citySearch.forEach(function (citySearch) {
+        // Create each list item
+        var liTag = document.createElement("li");
+        liTag.textContent = citySearch.cityName;
+        // liTag.classList = "list-unstyled";
+        // Display on page
+        var ulEl = document.getElementById("city-list");
+        ulEl.appendChild(liTag);
+    });
+}
+
+// Print Cities
+printCitySearches()
