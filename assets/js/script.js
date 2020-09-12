e7ee0b177442cf600c339335fcd96e8cdf785ddc
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-11 20:38:37 -0700

    Working on printing city searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,24 @@
 // Global Variables
 var cityNameEl = document.getElementById("city");
+var cityName = cityNameEl.value.trim();
 var searchBtn = document.getElementById("search-btn");
 var apiKey = "abc282c13673b28882968001a1c14445";
 
 // City Name, Date, Icon, Weather Conditions, Temperature, Humidity, Wind Speed
 
-// Display UV Index and color code 
-// fetch (
-//     `https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=${apiKey}`
-// )
-//     .then(function(response) {
-//         response.json();
-//         console.log(response);
-// })
+
+ // Display UV Index and color code 
+ fetch (
+     `https:api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=${apiKey}`
+ )
+     .then(function(response) {
+         response.json();
+         console.log(response);
+ })
 
 
 // Future 5-day weather for the city
 
-// City search is added to history
-function saveCity(event) {    
-    event.preventDefault();
-    var cityName = cityNameEl.value.trim();
-    // If city is not empty
-    if (city !== "") {
-        var citySave =
-        JSON.parse(window.localStorage.getItem("citySave")) || [];
-        
-        var cityInfo = {
-            name: cityName
-        };
-
-        // Save to Local Storage
-        citySave.push(cityInfo);
-        window.localStorage.setItem("citySave", JSON.stringify(citySave));
-    }
-    console.log(cityName)
-    getCity(cityName);
-}
 
 // Show Current Weather
 function getCity(cityName) {
@@ -61,47 +43,79 @@ function getCity(cityName) {
         console.log(weatherResponse.main.temp);
 
         // Append Card Contents to Card
-        currentCard.append(name, temp,);
+        // currentCard.append(name, temp, uvi);
+        currentCard.append(name);
+        currentCard.append(temp);
         
         // Append Card to Container
-        $("#today").append(currentCard);
 
         // Gather Lat & Lon
         var lat = weatherResponse.coord.lat;
         var lon = weatherResponse.coord.lon;
   
-        // Return a fetch request to get UV Index with the article title and rating parameters
-        return fetch(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apiKey}`
-        )
-        .then(function(uvResponse) {
-            return uvResponse.json();
-        })
-        .then(function(uvResponse) {
-            console.log(uvResponse);
-        })
-
+        getUV(lat, lon);
+        // currentCard.append(uvi);
+        $("#today").append(currentCard);
     })
+}
+
+// Return a fetch request to get UV Index with the article title and rating parameters
+var getUV = function(lat, lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    )
+    .then(function(uvResponse) {
+        return uvResponse.json();
+    })
+    .then(function(uvResponse) {
+        console.log(uvResponse);
+        var uvi = $("<p>").text(uvResponse.value);
+        console.log(uvi);
+        $("#today").append(uvi);
+    })
+    // return uvi;
 }
 
 // Submit City
 searchBtn.onclick = saveCity;
 
 
+// City search is added to history
+function saveCity(event) {    
+    event.preventDefault();
+    var cityName = cityNameEl.value.trim();
+    // If city is not empty
+    if (city !== "") {
+        var citySave =
+        JSON.parse(window.localStorage.getItem("citySave")) || [];
+        
+        var cityInfo = {
+            name: cityName
+        };
+
+        // Save to Local Storage
+        citySave.push(cityInfo);
+        window.localStorage.setItem("citySave", JSON.stringify(citySave));
+    }
+    console.log(cityName)
+    getCity(cityName);
+}
+
 // Show all Cities
-function printCitySearches() {
-    var citySearch = JSON.parse(window.localStorage.getItem("citySave")) || [];
-     
-    citySearch.forEach(function (citySearch) {
+function printCitySearches(cityName) {
+    // var citySearch = JSON.parse(window.localStorage.getItem("citySave")) || [];
+    // var cityName = cityNameEl.value.trim();
+    console.log("I'm inside printCitySearch" + cityName);
+    // citySearch.forEach(function (citySearch) {
         // Create each list item
         var liTag = document.createElement("li");
-        liTag.textContent = citySearch.cityName;
+        liTag.textContent = cityName;
         // liTag.classList = "list-unstyled";
         // Display on page
-        var ulEl = document.getElementById("city-list");
+        var ulEl = document.querySelector(".city-list");
         ulEl.appendChild(liTag);
-    });
+    // });
 }
 
 // Print Cities
-printCitySearches()
+printCitySearches(cityName);
